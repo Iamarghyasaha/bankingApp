32b1cbc0c8d5770a98a4d77e5f320e7cef3103df
arghya <sahaarghya5678@gmail.com>
2024-05-23 19:08:03 +0530

    Merge remote-tracking branch 'origin/main'

--- src/main/java/com/ndb/bankingApp/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ndb/bankingApp/exception/GlobalExceptionHandler.java
@@ -1,6 +1,4 @@
 package com.ndb.bankingApp.exception;
-
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
